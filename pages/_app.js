b54b8f0943f3fa2df5051c9e72bab47dbd6bf617
micahkatz <micahj2110@gmail.com>
2023-07-13 15:41:27 -0500

    added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import '../styles/globals.css';
+import { Analytics } from '@vercel/analytics/react';
 const queryClient = new QueryClient();
 
 export default function MyApp({ Component, pageProps }) {
     return (
         <QueryClientProvider client={queryClient}>
             <Component {...pageProps} />
+            <Analytics />
         </QueryClientProvider>
     );
 }
